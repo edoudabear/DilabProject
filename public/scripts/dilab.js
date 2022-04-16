705fa93f8eb664b30d4bc74e6e2e431e671d9c71
Eddie Cool <edou.aubert@gmail.com>
2022-04-16 23:17:25 +0200

    Fixed newly added function error

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -262,7 +262,7 @@ function updateLyrics(content) {
     nextBottom.style.opacity = "0";
     nextBottom.style.transform="translateY(20px)";
     nextBottom.classList.add("bottomText");
-    document.querySelector(".card").appendChild(nextBottom);
+    document.querySelector(".lyricsCard").appendChild(nextBottom);
     nextBottom.style.height="";
     setTimeout(()=>{
       nextBottom.style.opacity="";
