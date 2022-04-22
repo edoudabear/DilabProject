ab9414f7cf3133c9a3701a677d7824b5c456c380
root <root@diskloud.fr>
2022-04-22 21:29:27 +0200

    Lyrics debug 19

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -62,6 +62,7 @@ function parseLyrics(data) {
         j++;
         results[1].push(lyricsList[i].slice(j));
         if (i==0) {
+            console.log(lyricsList[i].slice(j));
             updateLyrics(lyricsList[i].slice(j));
         }
     }
