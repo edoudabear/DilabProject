2e0ccc914bb3f7eaa9665448e301c1f595ae36e0
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-05-01 22:28:18 +0200

    debugging

--- dilab_server_side.js
+++ dilab_server_side.js
@@ -136,6 +136,7 @@ app.post("/Dilab/:action", upload.array("files"), (req,res,err) => {
                             "data" : "internal server error",
                         }));
                 } if (results.length!=0) {
+                    console.log('ok');
                     res.end(JSON.stringify(results.flat()));
                 } else {
                     res.end('{ "return" : "ok", "status" : false, "description" : "internal server error (account is unfindable)" }');
