29620fd0581d7f66b959f16f667d3ab51be392a4
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-05-02 12:03:41 +0200

    Final fix

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -571,7 +571,7 @@ function pathAnalysis() {
                    data=data.data;
                    document.querySelector(".groupsWrapper").innerHTML="";
                    for (var i=0;i<data.length;i++) {
-                       document.querySelector(".groupsWrapper").innerHTML+=newGroupElement(data[i].groupName,"",data[i].description,new Date(data[i].dateOfBirth).getFullYear(),data[i].nCollaborators,"",data[i].groupPicture);
+                       document.querySelector(".groupsWrapper").innerHTML+=newGroupElement(data[i].groupName,"",data[i].description,new Date(data[i].dateOfBirth).getFullYear(),data[i].nCollaborators,"","",data[i].groupPicture);
                     }
                 });
                 if (!document.querySelector(".loginButton")) {
