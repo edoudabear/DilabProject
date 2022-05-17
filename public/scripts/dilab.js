99f4daae77dd96e573286b0be176ac362b8f048b
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-05-17 23:13:16 +0200

    New fix

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -571,7 +571,7 @@ function pathAnalysis() {
                                 el.classList.add("project");
                                 el.innerHTML+=newProjectElement(line.name,line.groupName,line.description,line.dateOfBirth,line.nCollaborators,line.projectPicture,line.audioFile);
                                 document.querySelector(".projectsWrapper").appendChild(el);
-                                progress(line.currentPhase,el);
+                                setTimeout(()=>{progress(line.currentPhase,el);},0);
                             }
                         }
                     }
