0a60861476522f6cd6fe957027a99424264f8c91
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-06-02 10:23:00 +0200

    Update fix

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -665,7 +665,7 @@ function pathAnalysis() {
                                 var line=projectList[i],
                                 el=document.createElement("div");
                                 el.classList.add("project");
-                                var projectPPath=(line.audioFileDir!=null) ? `https://e.diskloud.fr/Dilab/project/${line.groupName}/${line.name}` : "https://e.diskloud.fr/Dilab/project/disc.svg"
+                                var projectPPath=(line.projectPicture!=null) ? `https://e.diskloud.fr/Dilab/project/${line.groupName}/${line.name}` : "https://e.diskloud.fr/Dilab/project/disc.svg"
                                 el.innerHTML+=newProjectElement(line.name,projectPPath,line.groupName,line.description,line.dateOfBirth,line.nCollaborators,line.projectPicture,line.audioFile);
                                 el.setAttribute("onclick",`loadPage("${line.name} Dilab","project",[["p","${line.name}"],["g","${line.groupName}"]]);`)
                                 document.querySelector(".projectsWrapper").appendChild(el);
