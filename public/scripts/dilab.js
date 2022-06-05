d0f94d46194b2066d399cf479b5291f7279a2e1e
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-06-05 09:03:07 +0200

    client update

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -161,8 +161,7 @@ function loadSound(url) {
             while (!playEl.classList.contains("playlistElement") && !playEl.classList.contains("playlistMenu")) {
                 playEl=playEl.parentNode;
             }
-            console.log(playEl);
-            var index=e.target.getAttribute("dataval");
+            var index=playEl.getAttribute("dataval");
             contextMenu.innerHTML+="<div onclick=\"playSound("+index+",true)\" class=\"menuOption\">Play</div>";
             contextMenu.innerHTML+="<div onclick=\"removePlaylistElement("+index+")\" class=\"menuOption\">Remove from queue</div>";
             var menuElement=document.querySelector(".contextMenu");
