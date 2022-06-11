cbeadd420145d7fa3db1e8f0a8d161cea47b65b2
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-06-11 16:47:43 +0200

    Server side update

--- dilab_server_side.js
+++ dilab_server_side.js
@@ -614,7 +614,7 @@ app.post("/Dilab/:action", upload.array("files"), (req,res,err) => {
             }
         }
     } else if (req.params.action=="set") {
-        if (req.body.type=="newJoinRequest" && req.body.groupName) {
+        if (req.body.type=="newJoinRequest" && req.body.groupName && req.session.dilab) {
             dilabConnection.query(`INSERT INTO DilabMembersWaitList (waiter,groupId) SELECT ${req.session.dilab},id FROM DilabMusicGroups WHERE groupName=${dilabConnection.escape(req.body.groupName)} LIMIT 1`,(err,results,fields) => {
                 if (err) {
                     res.end(JSON.stringify(
