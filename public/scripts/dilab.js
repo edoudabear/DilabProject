24d794f2dfe2c6b863f2b3f3ec4014dc8cc4b582
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-06-29 22:57:50 +0200

    Client chat get update

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -740,7 +740,7 @@ function pathAnalysis() {
                                             } else {
                                                 var date1=new Date(log.data[i-1].sendDate),
                                                 date2=new Date(log.data[i].sendDate);
-                                                if (date1.getDay()!=date2.getDay() || date1.getMonth()!=date2.getMonth() || date1.getFullYear()!=date2.getFullYear()) {
+                                                if (date1.getDate()!=date2.getDate() || date1.getMonth()!=date2.getMonth() || date1.getFullYear()!=date2.getFullYear()) {
                                                     document.querySelector(".messagesContainer").innerHTML+=generateNewDateAnouncement(log.data[i].sendTime)
                                                 }
                                             }
