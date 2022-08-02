9d6984430c73239a73632f4d530388143d6e8f80
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-08-02 18:56:49 +0200

    Date issue mostly fixed

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -2461,10 +2461,8 @@ function updateChat(groupName,projectName=null) {
                     } else {
                         var date1=new Date(lastMessage.sendTime),
                         date2=new Date(log.data[i].sendTime);
-                        console.log("Comparison 1 about to be done");
                         if (date1.getDate()!=date2.getDate() || date1.getMonth()!=date2.getMonth() || date1.getFullYear()!=date2.getFullYear()) {
-                            document.querySelector(".messagesContainer").innerHTML+=generateNewDateAnouncement(log.data[0].sendTime)
-                            console.log("Comparison 1 turned out to be true");
+                            document.querySelector(".messagesContainer").innerHTML+=generateNewDateAnouncement(log.data[i].sendTime)
                         }
                     }
                 } else {
