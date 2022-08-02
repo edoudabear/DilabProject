1fac5c33aeccb75e6ee819405b1d36d5dec4c61d
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-08-02 20:50:27 +0200

    First fix

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -1941,10 +1941,10 @@ function pathAnalysis() {
                                 data= JSON.parse(data.data);
                                 // Release results
                                 for (var i=0;i<data[0].length;i++) {
-                                    document.querySelector(".groupsWrapper").innerHTML+=newReleaseElement(data[0][i].name,data[0][i].groupName,new Date(data[0][i].releaseDate).getFullYear(),data[0][i].nb_streams+" streams",duree,data[0][i].releasePicture,data[0][i].filePath);
+                                    document.querySelector(".releases").innerHTML+=newReleaseElement(data[0][i].name,data[0][i].groupName,new Date(data[0][i].releaseDate).getFullYear(),data[0][i].nb_streams+" streams",duree,data[0][i].releasePicture,data[0][i].filePath);
                                 }
                                 if (data[0].length==0) {
-                                    document.querySelector(".groupsWrapper").innerHTML="<span class=\"noDataTextInfo\">No projects found :(<p></div>";
+                                    document.querySelector(".releases").innerHTML="<span class=\"noDataTextInfo\">No projects found :(<p></div>";
                                 }
                                 // Group results
                                 for (var i=0;i<data[2].length;i++) {
