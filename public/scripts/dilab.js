68bf5105fd8eea82551c5b9e3a8729175aa18499
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-08-06 14:30:11 +0200

    Debug

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -3116,6 +3116,7 @@ function escapeHtml(unsafe) {
 // Element templates
 
 function newReleaseElement(title,group,releaseDate,streams,duration,imagePath,filePath) {
+    console.log(filepath);
     return `
     <div dataPath="${encodeURIComponent(filePath)}" class=release>
         <div class=left-content>
