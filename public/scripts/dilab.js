c40f1b12162b9a74af58629b58a4d57bef039183
root <root@diskloud.fr>
2024-05-04 13:23:49 +0200

    Minor update

--- public/scripts/dilab.js
+++ public/scripts/dilab.js
@@ -923,7 +923,7 @@ function pathAnalysis() {
                                                                     return out.json();
                                                                 }).then(log => {
                                                                     if (log.status===false) {
-                                                                        Swal.fire("Error","Something went wrong : the server responded unexpectedly. Please try later","error");
+                                                                        Swal.fire("Error","Something went wrong : the server responded unexpectedly. Please try later : "+log.data,"error");
                                                                         console.log("An error occured while checking if the user was a member, waiting, or nothing at all");
                                                                         return;
                                                                     } else {
